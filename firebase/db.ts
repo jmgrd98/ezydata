465e2de72c80b738e49fb527a761313296893cc2
jmgrd98 <jmgrd98@gmail.com>
2025-01-28 12:40:29 -0300

    deploy fix

--- firebase/db.ts
+++ firebase/db.ts
@@ -1,9 +1,7 @@
-// db.ts
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBZIHh4LbNVoWmDzpJG8Vyhltdb6H9qxlk",
@@ -20,7 +18,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
-const analytics = getAnalytics(app);
 
 export interface Project {
   id?: string;
@@ -31,4 +28,4 @@ export interface Project {
   createdAt?: Date;
 }
 
-export { app, db, auth, storage, analytics };
+export { app, db, auth, storage };
