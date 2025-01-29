f81874185c9d0bfafb57696710b43d9472bebbc3
jmgrd98 <jmgrd98@gmail.com>
2025-01-28 22:11:09 -0300

    translation landing video

--- components/Landing/landing-video.tsx
+++ components/Landing/landing-video.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react'
+import { useTranslation } from 'react-i18next';
+import i18n from "@/translation";
 
 const LandingVideo = () => {
+  const { t } = useTranslation();
   const [country, setCountry] = React.useState<string>('');
   useEffect(() => {
     detectLanguage();
@@ -12,20 +15,22 @@ const LandingVideo = () => {
       const data = await response.json();
       const countryCode = data.country_code;
       setCountry(countryCode);
-      // const languageMap: Record<string, string> = {
-      //   US: "en",
-      //   BR: "pt",
-      // };
+      const languageMap: Record<string, string> = {
+        US: "en",
+        BR: "pt",
+      };
   
-      // const language = languageMap[countryCode] || "en";
-      // i18n.changeLanguage(language);
+      const language = languageMap[countryCode] || "en";
+      i18n.changeLanguage(language);
     } catch (error) {
       console.error("Error detecting location:", error);
     }
   };
   return (
     <div className='w-full h-full bg-black text-white montserrat text-center flex flex-col items-center'>
-      <p className='text-4xl'>Veja como funciona</p>
+      <p className='text-4xl'>
+        {t('landing.landingVideo.title')}
+      </p>
       {country === 'US' ? <video 
         src="/assets/video-ingles.mp4"
         width={'70%'} 
