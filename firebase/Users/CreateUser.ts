023c7fa872225c04299a5ed5f9d650f0e8cccfea
jmgrd98 <jmgrd98@gmail.com>
2025-02-06 13:02:51 -0300

    testing stripe webhook

--- firebase/Users/CreateUser.ts
+++ firebase/Users/CreateUser.ts
@@ -1,13 +1,19 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 import { db, User } from "../db";
 
 interface ICreateUserProps {
-    userId: string;
-    data: User;
+  userId: string;
+  data: User;
 }
 
 export default async function CreateUser({ userId, data }: ICreateUserProps) {
-    const userRef = doc(db, "users", userId);
-    await setDoc(userRef, data);
-    return { message: "User created successfully", userId };
+  const userRef = doc(db, "users", userId);
+  
+  const docSnap = await getDoc(userRef);
+  if (docSnap.exists()) {
+    return { message: "User already exists", userId };
+  }
+  
+  await setDoc(userRef, data);
+  return { message: "User created successfully", userId };
 }
