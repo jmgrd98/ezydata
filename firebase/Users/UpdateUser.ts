4ae94819ef9b369077a14b706e598b2e32358df8
jmgrd98 <jmgrd98@gmail.com>
2025-02-10 10:24:20 -0300

    commit

--- firebase/Users/UpdateUser.ts
+++ firebase/Users/UpdateUser.ts
@@ -2,15 +2,14 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db, User } from "../db";
 
 interface IUpdateUserProps {
-    userId: string;
-    data: Partial<User>;
+  userId: string;
+  data: Partial<User>;
 }
 
 export default async function UpdateUser({ userId, data }: IUpdateUserProps) {
-    console.log('ENTROU UPDATE USER')
-    const userRef = doc(db, "users", userId);
+  const userRef = doc(db, "users", userId);
 
-    await updateDoc(userRef, data);
+  await updateDoc(userRef, data);
 
-    return { message: "User updated successfully", userId };
+  return { message: "User updated successfully", userId };
 }
